23f0693622a3427feb3c5ce37a67586f50fa1a11
IvanovOleg <olivanov87@gmail.com>
2019-10-09 11:17:16 +0300

    Added the HelloWorld application

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,36 @@
+# print('Hello World!')
+
+# print (1 + 2)
+
+# print ("The End")
+# print ('Include "quote" in string')
+
+
+# print ("hello" + " world")
+
+
+# greeting = "Hello"
+# name = "Oleg"
+
+# # if we want a space, we can add it inside single quotes
+# print(greeting + ' ' + name)
+
+
+############################################################################
+
+# greeting = "Hello"
+# name = input("Please enter your name: ")
+# print (greeting + ' ' + name)
+
+# splitString = "This string has been\nsplit over\nseveeral\nlines"
+# print(splitString)
+
+# tabbedString = "1\t2\t3\t4"
+# print(tabbedString)
+
+print('Pet shop owner said "No, no,\'e\'s uh, ...he\s resting"')
+
+anotherSplitString = """ This string has been
+split over
+several lines """
+print(anotherSplitString)
