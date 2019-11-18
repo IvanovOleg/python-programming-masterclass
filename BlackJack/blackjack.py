fe0f15e149402967fc016774a58abed86c255a55
Oleg Ivanov <olivanov87@gmail.com>
2019-11-18 16:14:56 +0200

    Added blackjack

--- BlackJack/blackjack.py
+++ BlackJack/blackjack.py
@@ -22,7 +22,7 @@ def load_images(card_images):
             card_images.append((10, image,))
 
 
-def deal_card(frame):
+def _deal_card(frame):
     # pop the next card off the top of the deck
     next_card = deck.pop(0)
     # add the image to a Label and display the label
@@ -55,7 +55,7 @@ def deal_dealer():
     dealer_score = score_hand(dealer_hand)
     while 0 < dealer_score < 17:
         dealer_score = score_hand(dealer_hand)
-        dealer_hand.append(deal_card(dealer_card_frame))
+        dealer_hand.append(_deal_card(dealer_card_frame))
         dealer_score_label.set(dealer_score)
 
     player_score = score_hand(player_hand)
@@ -71,7 +71,7 @@ def deal_dealer():
 
 
 def deal_player():
-    player_hand.append(deal_card(player_card_frame))
+    player_hand.append(_deal_card(player_card_frame))
     player_score = score_hand(player_hand)
     player_score_label.set(player_score)
     if player_score > 21:
@@ -95,6 +95,12 @@ def shuffle():
     random.shuffle(deck)
 
 
+def initial_deal():
+    deal_player()
+    dealer_hand.append(_deal_card(dealer_card_frame))
+    dealer_score_label.set(score_hand(dealer_hand))
+    deal_player()
+
 def new_game():
     global dealer_card_frame
     global player_card_frame
@@ -112,13 +118,14 @@ def new_game():
     result_text.set("")
 
     dealer_hand = []
-    player_hand = []
-    
-    deal_player()
-    dealer_hand.append(deal_card(dealer_card_frame))
-    dealer_score_label.set(score_hand(dealer_hand))
-    deal_player()
+    player_hand = [] 
 
+    initial_deal()
+
+
+def play():
+    initial_deal()
+    mainWindow.mainloop()
 
 mainWindow = tkinter.Tk()
 
@@ -181,10 +188,10 @@ shuffle()
 # Create a list to store dealer's and player's hands
 dealer_hand = []
 player_hand = []
-new_game()
-
-mainWindow.mainloop()
 
 # Challenge: Create a new button with text "New Game", that calls function that
 # clears cards from screen, resets hands. Easies way of resetting a frame is to
 # destroy it and recreate with the same name.
+
+if __name__ == '__main__':
+    play()
---
+++ BlackJack/import_test.py
@@ -0,0 +1,16 @@
+import blackjack
+
+# gl = sorted(globals())
+
+# for x in gl:
+#     print(x)
+
+
+# print(__name__)
+
+# blackjack._deal_card(blackjack.dealer_card_frame)
+# blackjack.play()
+
+personal_details = ("Tim", 24, "Australia")
+name, _, country = personal_details
+print(name, country)
